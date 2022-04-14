ea5e6c7d339fcd2ad5bdd28039c3cf4d4f128a75
femogo <102486584+femogo@users.noreply.github.com>
2022-04-14 22:35:34 +0200

    Añadida la clase Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiCSherpas.Models
+{
+    public class Clientes
+    {
+
+        public int Id { get; set; }
+
+        public String? Name { get; set; }
+        public String? Surname { get; set; }
+
+
+        public String? Email { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
